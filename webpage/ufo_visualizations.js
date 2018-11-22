fa7cd1d8cf923a6bde4f39ea03bd7d534ca9384c
xuezzou <ilfam1999@gmail.com>
2018-11-22 01:59:39 -0800

    added subplot2

--- webpage/ufo_visualizations.js
+++ webpage/ufo_visualizations.js
@@ -30,11 +30,11 @@ function plot_it() {
     }).entries(ufo_data);
 
 
-    d3.selectAll('body').append('svg').attr('width', width).attr('height', height);
+    let svg = d3.selectAll('body').append('svg').attr('width', width).attr('height', height);
     //add map section
     //map_data.features = map_data.features.filter(d => d.properties.STATAE=='037'); 
-    projection = d3.geoAzimuthalEqualArea().rotate([90, 0]).fitSize([map_width, map_height], map_data);
-    geo_generator = d3.geoPath().projection(projection);
+    let projection = d3.geoAzimuthalEqualArea().rotate([90, 0]).fitSize([map_width, map_height], map_data);
+    let geo_generator = d3.geoPath().projection(projection);
 
     let mapPlot = d3.select('svg').append('g').attr('id', 'map_plot').attr('transform', "translate(" + padding + "," + padding + ")");
 
@@ -63,9 +63,21 @@ function plot_it() {
     });
     let projected_reports = all_reports.map(d => projection(d));
 
+    let viewCircle = false;
+    // change view
+    svg.append('text').text('details')
+        .attr('x', 10)
+        .attr('y', 10)
+        .on("click", function() {
+        if(!viewCircle) {
+            plotCircles();
+            viewCircle = true;
+        } else {
+            removeCircles();
+            viewCircle = false;
+        }
+    });
 
-
-    plotCircles();
 
     function plotCircles() {
         // adjust the background
@@ -80,6 +92,11 @@ function plot_it() {
             .attr('stroke-width', 0.1)
             .attr('stroke-opacity', 0.1)
             .attr('fill-opacity', '0.2');
+
+        // allow zooming effect
+        mapPlot.call(d3.zoom().on("zoom", function () {
+            mapPlot.attr("transform", d3.event.transform)
+        }));
     }
 
     function removeCircles() {
@@ -98,7 +115,7 @@ function plot_it() {
     d3.selectAll('#subplot1').append('rect').attr('x', 0).attr('y', 0).attr('width', sub_info_width).attr('height', sub_info_height).attr('fill', 'blue').attr('opacity', opacity)
 
 
-    d3.selectAll('#info_plot').append('g').attr('id', 'subplot2').attr('transform', "translate(" + padding + "," + (sub_info_height + 2 * padding) + ")")
+    let shapePlot = d3.selectAll('#info_plot').append('g').attr('id', 'subplot2').attr('transform', "translate(" + padding + "," + (sub_info_height + 2 * padding) + ")")
     d3.selectAll('#subplot2').append('rect').attr('x', 0).attr('y', 0).attr('width', sub_info_width).attr('height', sub_info_height).attr('fill', 'blue').attr('opacity', opacity)
 
     d3.selectAll('#info_plot').append('g').attr('id', 'subplot3').attr('transform', "translate(" + padding + "," + (2 * sub_info_height + 3 * padding) + ")")
@@ -108,11 +125,6 @@ function plot_it() {
     // plot the sub plots
     states.on("click", function (d) {
         let selectedState = d.properties['NAME'];
-        plotSubTimePlot(selectedState);
-    });
-
-
-    function plotSubTimePlot(selectedState) {
 
         selectedStateData = aggregatedByState.find(function (element) {
             return element.key == selectedState;
@@ -121,6 +133,13 @@ function plot_it() {
         if (!selectedStateData) {
             return;
         }
+
+        plotSubTimePlot(selectedState, selectedStateData);
+        plofSubShapePlot(selectedState, selectedStateData);
+    });
+
+
+    function plotSubTimePlot(selectedState, selectedStateData) {
 
         let selectedStateByTime = d3.nest().key(function (d) {
             return d.year;
@@ -165,7 +184,7 @@ function plot_it() {
 
         timePlot.append('text').attr('class', 'scale')
             .text(selectedState)
-            .attr("x", sub_info_width/2)
+            .attr("x", sub_info_width / 2)
             .attr("y", -10)
             .attr("dy", "0.32em")
             .attr("fill", "#000")
@@ -198,6 +217,70 @@ function plot_it() {
 
     }
 
+    function plofSubShapePlot(selectedState, selectedStateData) {
+
+        // notes the number of categories in bar chart
+        let viewAmount = 6;
+        let padding = 0.3;
+
+        let selectedStateByShape = d3.nest().key(function (d) {
+            return d.shape;
+        }).rollup(function (leaves) {
+            return leaves.length;
+        }).entries(selectedStateData.values)
+            .sort(function(a, b){ return d3.descending(a.value, b.value); });
+
+        selectedStateByShape = selectedStateByShape.slice(0, viewAmount);
+
+        // console.log(selectedStateByShape);
+
+        // set up the axes
+        let x = d3.scaleBand().domain(selectedStateByShape.map(d => d.key)).rangeRound([0, sub_info_width]).padding(padding),
+            y = d3.scaleLinear().domain([0, d3.max(selectedStateByShape, d => d.value)]).rangeRound([sub_info_height, 0]);
+
+        // draw the plot/rects
+        let update = shapePlot.selectAll(".bar")
+            .data(selectedStateByShape);
+
+
+        update.enter().append("rect")
+            .attr('class', 'bar')
+            .attr("fill", "#800026")
+            .attr("x", d => x(d.key))
+            .attr("y", d => y(d.value))
+            .attr("width", x.bandwidth())
+            .attr("height", function (d) {
+                return sub_info_height - y(d.value);
+            });
+        // console.log(update);
+        update.remove();
+        shapePlot.selectAll(".scale").remove();
+
+
+        shapePlot.append("g").attr('class', 'scale')
+            .attr("transform", "translate(0," + sub_info_height + ")")
+            // .style('font-size', 8)
+            .call(d3.axisBottom(x))
+            .append("text")
+            .attr("x", sub_info_width + 4)
+            .attr("dy", "0.32em")
+            .attr("fill", "#000")
+            .attr("font-weight", "bold")
+            .attr("text-anchor", "start")
+            .text("Shape");
+
+        shapePlot.append("g").attr('class', 'scale')
+            .call(d3.axisLeft(y))
+            .append("text")
+            .attr("x", 4)
+            .attr('y', -6)
+            .attr("dy", "0.32em")
+            .attr("fill", "#000")
+            .attr("font-weight", "bold")
+            .attr("text-anchor", "start")
+            .text("Count");
+
+    }
 
 
 }
